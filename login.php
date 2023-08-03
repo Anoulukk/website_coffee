b9d33b340cdf934474754e5d0022c6b330785a91
Anoulukk <101563242+Anoulukk@users.noreply.github.com>
2023-08-03 14:05:14 +0700

    change color error

--- login.php
+++ login.php
@@ -1,8 +1,33 @@
+<script>
+    // Function to hide the error message after 3 seconds
+    function hideErrorMessage() {
+      var errorMessage = document.getElementById('error-message');
+      if (errorMessage) {
+        setTimeout(function() {
+          errorMessage.style.display = 'none';
+        }, 3000); // 3 seconds
+      }
+    }
 
+    // Call the function on page load
+    window.onload = function() {
+      hideErrorMessage();
+    };
+  </script>
+   <style>
+    /* Add the CSS style to change the color of the error message to red */
+    .error-message {
+      color: red;
+    }
+    </style>
 <?php
 include('config.php'); 
 include ('footerheader.php');
-
+$error_message = '';
+if (isset($_SESSION['error'])) {
+  $error_message = $_SESSION['error'];
+  unset($_SESSION['error']);
+}
 ?>
 <body>
      <!-- login -->
@@ -19,15 +44,16 @@ include ('footerheader.php');
               <div class="form-control">
         <input type="password" name="password" class="form-control" required placeholder="password">
           </div>
+          <div class="error-message" id="error-message">
+              <?php
+              // Display the error message if set
+              echo $error_message;
+              ?>
+            </div>
         <input type="submit" name="submit" value="login" class="btn btn-primary">
       </form>
               <a href="register.php">Don't have an account? Sign up</a>
-              <?php
-  // Display the error message if set
-  if (isset($error_message)) {
-    echo '<p>' . $error_message . '</p>';
-  }
-  ?>
+
             <br>
           </div>
         </div>
